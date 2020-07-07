32294df8988bfcae7590be3d848f3a0d6c4fc248
Jannat-Randhawa <58931852+Jannat-Randhawa@users.noreply.github.com>
2020-07-07 00:56:42 -0400

    config.js file added

--- static/js/logic.js
+++ static/js/logic.js
@@ -2,3 +2,10 @@
 var EarthquakeURL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_month.geojson";
 
 // set layers for the map 
+var streetMap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "mapbox/light-v10",
+    accessToken: API_KEY
+  }).addTo(myMap);
+  
