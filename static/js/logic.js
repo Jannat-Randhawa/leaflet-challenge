493d8040a9de9bcc8a3d2dc3a80dda176e8d273d
Jannat-Randhawa <58931852+Jannat-Randhawa@users.noreply.github.com>
2020-07-07 00:52:54 -0400

    basic code

---
+++ static/js/logic.js
@@ -0,0 +1,4 @@
+// variable to store GeoJSON data for the earthquake URL 
+var EarthquakeURL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_month.geojson";
+
+// set layers for the map 
